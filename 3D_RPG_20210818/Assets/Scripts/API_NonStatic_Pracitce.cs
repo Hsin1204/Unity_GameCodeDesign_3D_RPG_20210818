b1435ba6d15e7b51bab80aff381913513db7b0c3
KoHsin <89112641+Hsin1204@users.noreply.github.com>
2021-09-27 14:53:59 +0800

    API非靜態練習

---
+++ 3D_RPG_20210818/Assets/Scripts/API_NonStatic_Pracitce.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API_NonStatic_Pracitce : MonoBehaviour
+{
+    public Camera cam;
+    public SpriteRenderer sr;
+    public SpriteRenderer sr_rotate, sr_fly;
+    Rigidbody2D fly_rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        print("攝影機深度 : " + cam.depth);
+        print("圖片顏色" + sr.color);
+
+        //設定攝影機背景顏色
+        cam.backgroundColor = Random.ColorHSV();
+
+        //圖片上下翻轉
+        sr.flipY = true;
+
+
+        fly_rigid = sr_fly.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //第一張圖片旋轉
+        sr_rotate.transform.Rotate(Vector3.forward, 0.1f);
+
+        //第二張圖片往上飛
+        fly_rigid.AddForce(Vector2.up* 5f);
+    }
+}
