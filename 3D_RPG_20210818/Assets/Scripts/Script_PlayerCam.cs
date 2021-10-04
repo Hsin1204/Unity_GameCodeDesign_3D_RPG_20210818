ca03273bd83501ff3fa75fb8f1369cea4e175474
KoHsin <89112641+Hsin1204@users.noreply.github.com>
2021-10-04 11:59:37 +0800

    新增攝影機旋轉功能

--- 3D_RPG_20210818/Assets/Scripts/Script_PlayerCam.cs
+++ 3D_RPG_20210818/Assets/Scripts/Script_PlayerCam.cs
@@ -13,18 +13,53 @@ namespace Player
         [Header("目標物件")]
         public Transform target;
 
-        [Header("追蹤速度")]
+        [Header("追蹤速度"),Range(0,100)]
         public float trackSpeed = 1.5f;
+
+        [Header("左右旋轉速度"),Range(0,100)]
+        public float turnSpeed_Horizontal = 5;
+
+        [Header("上下旋轉速度"), Range(0, 100)]
+        public float turnSpeed_Vertical = 5;
 
         #endregion
 
         #region 屬性
+        //取得滑鼠水平座標
+        private float inputMouseX { get => Input.GetAxis("Mouse X"); }
+        //取得滑鼠垂直座標
+        private float inputMouseY { get => Input.GetAxis("Mouse Y"); }
         #endregion
 
         #region 事件
         #endregion
 
         #region 方法
+        private void LateUpdate() //這會在 Update與Fixed Update後執行
+        {
+            TrackTarget();
+            RotateCam();
+            
+
+        }
+        private void TrackTarget()
+        {
+            Vector3 posTarget = target.position; // 取得目標座標
+            Vector3 posCam = transform.position; //取得攝影機座標
+
+            //如果沒有乘上一個frame的執行時間，在不同的設備間可能會產生很大的速度差異
+            posCam = Vector3.Lerp(posCam, posTarget, trackSpeed * Time.deltaTime); //攝影機座標 = 插植
+            
+            transform.position = posCam; //將新的攝影機座標賦值回去
+        }    
+        private void RotateCam()
+        {
+            transform.Rotate(
+                inputMouseY *turnSpeed_Vertical*Time.deltaTime,
+                inputMouseX *turnSpeed_Horizontal*Time.deltaTime,
+                0);
+        }
+
         #endregion
 
     }
