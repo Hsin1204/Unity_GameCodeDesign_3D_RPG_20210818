3b91a98d0f4b9d8fd33fbedfda3faf0768338cb0
KoHsin <89112641+Hsin1204@users.noreply.github.com>
2021-10-21 12:00:15 +0800

    建構NPC對話系統資料

---
+++ 3D_RPG_20210818/Assets/Scripts/NPC_Script/Script_DataDialogue.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Dialogue
+{
+    /// <summary>
+    /// 對話系統資料
+    /// NPC要對話的三個階段內容
+    /// 接任務前、進行中、完成任務
+    /// </summary>
+    //ScriptableObject 繼承此類別會變成腳本化物件
+    //可將此腳本資料當成物件保存在專案Peoject內
+    //CreateAssetMenu 類別屬性:為此類別建立專案內選單
+    //menuName 選單名稱，可用/分層
+    //fileName 生成的檔案名稱
+    [CreateAssetMenu(menuName = "Cus/對話資料", fileName = "NPC對話資料")]
+    public class Script_DataDialogue : ScriptableObject
+    {
+        [Header("任務前對話內容"), TextArea(3, 7)]
+        public string[] beforeMission;
+        [Header("任務中對話內容"), TextArea(3, 7)]
+        public string[] inMission;
+        [Header("任務後對話內容"), TextArea(3, 7)]
+        public string[] FinMission;
+        [Header("任務需求數量"), Range(0, 10)]
+        public int missionCount;
+
+        //使用列舉:
+        //語法 : 修飾詞 列舉名稱 自定義欄位名稱;
+        [Header("NPC任務狀態")]
+        public NPC_MissionState NPC_MS = NPC_MissionState.BeforeMission;
+
+    }
+
+}
---
+++ 3D_RPG_20210818/Assets/Scripts/NPC_Script/Script_NPC_State.cs
@@ -0,0 +1,10 @@
+namespace Dialogue
+{
+    // 列舉 : enum(enumeration)下拉式選單，可自行定義選項
+    // 語法 : 修飾詞 列舉 列舉名稱{列舉內容1,....,列舉內容n}
+
+    public enum NPC_MissionState
+    {
+        BeforeMission,InMission,FinMission
+    }
+}
