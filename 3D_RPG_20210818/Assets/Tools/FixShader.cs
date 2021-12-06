45d853f938ad8ffdb2c17f735700414344406fae
KoHsin <89112641+Hsin1204@users.noreply.github.com>
2021-12-06 10:05:35 +0800

    完成後製效果

--- 3D_RPG_20210818/Assets/Tools/FixShader.cs
+++ 3D_RPG_20210818/Assets/Tools/FixShader.cs
@@ -1,3 +1,4 @@
+#if (UNITY_EDITOR)
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -136,4 +137,4 @@ public class FixShader : MonoBehaviour
         return temp;
     }
 }
-
+#endif
